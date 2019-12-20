a111b4567279ef80d4ee5227de4c5c8dcdc46333
koehlersimon <simonkohler@192.168.2.1>
2019-12-20 12:44:59 -0500

    Changed way of registering a module according to new TYPO3 docs

--- ext_tables.php
+++ ext_tables.php
@@ -17,6 +17,7 @@ call_user_func(
 
 
             // The main site module for slug editing
+            /*
             \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
                 'SIMONKOEHLER.Slug',
                 'site',
@@ -25,6 +26,23 @@ call_user_func(
                 [
                     'Page' => 'list,site,tree',
                     'Extension' => 'additionalTable',
+                ],
+                [
+                    'access' => 'user,group',
+                    'icon'   => 'EXT:slug/Resources/Public/Icons/slug-be-module.svg',
+                    'labels' => 'LLL:EXT:slug/Resources/Private/Language/locallang_slugs.xlf',
+                ]
+            );
+            */
+
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                'Slug',
+                'site',
+                'slugs',
+                '',
+                [
+                    \SIMONKOEHLER\Slug\Controller\PageController::class => 'list,site,tree',
+                    \SIMONKOEHLER\Slug\Controller\ExtensionController::class => 'additionalTable'
                 ],
                 [
                     'access' => 'user,group',
