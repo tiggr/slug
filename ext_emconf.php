50d33b6b3da4c1dd7c70e9c882206e1eee7440f7
koehlersimon <simonkohler@192.168.2.1>
2019-12-20 12:49:20 -0500

    Changed to BETA and require TYPO3 10.2!

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,12 +7,12 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Simon Köhler',
     'author_email' => 'info@simon-koehler.com',
     'company' => 'simon-koehler.com',
-    'state' => 'stable',
+    'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '2.0.2',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.2.99',
+            'typo3' => '10.2.0-10.2.99',
         ],
         'conflicts' => [],
         'suggests' => [],
